cc375fc9bba30e71e277428d8775a125fc715789
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-08-23 11:29:54 +0100

    combined survival curve for ITGB3+ITGAE+CD3E+CD8A+ in melanoma

--- ITGB3_ITGAE_survival.R
+++ ITGB3_ITGAE_survival.R
@@ -11,6 +11,7 @@ library(survival)
 #------ Skin cutaneous melanoma
 # follow tutorial for downloading dataset https://bioc.ism.ac.jp/packages/3.2/bioc/vignettes/RTCGA/inst/doc/RTCGA_Tutotial.html
 # follow tutorial for survival curve generation http://www.sthda.com/english/wiki/survival-analysis-basics
+# follow tutorial for setting group of genes https://github.com/kassambara/survminer/issues/41
 
 # # download clinical data (default)
 # downloadTCGA(cancerTypes = "SKCM", destDir = "tcga")
@@ -78,3 +79,48 @@ ggsurvplot(fit,
            surv.median.line = "hv", # Specify median survival
            ggtheme = theme_bw(), # Change ggplot2 theme
            palette = c("#E7B800", "#2E9FDF"))
+
+# genes of interest - ITGB3, ITGAE, CD8A, CD3E
+genes <- rna[,colnames(rna) %like% "ITGB3|ITGAE|CD8A|CD3",]
+genes <- genes[,c(19, 21, 22, 24)]
+# remove numbers after gene names (Entrez IDs?)
+colnames(genes) <- gsub("\\|.*", "", colnames(genes))
+genes$Patient.ID <- rna$patient.bcr_patient_barcode
+
+# merge clinical and genes data
+mel <- clin[,c("Patient.ID", "time", "status")]
+mel <- merge(mel, genes, by="Patient.ID")
+
+# determine optimal cutpoint of variables
+res.cut <- surv_cutpoint(mel, time = "time", event = "status", 
+                         variables = c("ITGB3", "ITGAE", "CD8A", "CD3E"))
+summary(res.cut)
+
+# plot cutpoint for ITGAE
+# palette = "npg" (nature publishing group), see ?ggpubr::ggpar
+plot(res.cut, "CD3E", palette = "npg")
+
+# categorise variables
+res.cat <- surv_categorize(res.cut)
+head(res.cat)
+
+# add new columns 
+# DP - double positive - ITGB3+ITGAE+
+# TP - triple positive - ITB3+ITGAE+CD3E+
+# AP - all positive - ITGB3+ITGAE+CD3E+CD8A+
+# numerical values from summary(res.cut)
+mel$DP <- ifelse(mel$ITGB3 > 6992.3114 & mel$ITGAE > 123.5888, "high", "low")
+mel$TP <- ifelse(mel$ITGB3 > 6992.3114 & mel$ITGAE > 123.5888 & mel$CD3E > 279.4512, "high", "low")
+mel$AP <- ifelse(mel$ITGB3 > 6992.3114 & mel$ITGAE > 123.5888 & mel$CD3E > 279.4512 & mel$CD8A > 554.4929, "high", "low")
+
+dp_fit <- survfit(Surv(time, status) ~DP, data = mel)
+dp_plot <- ggsurvplot(dp_fit, risk.table = TRUE, conf.int = TRUE, pval = TRUE)
+dp_plot
+
+tp_fit <- survfit(Surv(time, status) ~TP, data = mel)
+tp_plot <- ggsurvplot(tp_fit, risk.table = TRUE, conf.int = TRUE, pval = TRUE)
+tp_plot
+
+ap_fit <- survfit(Surv(time, status) ~AP, data = mel)
+ap_plot <- ggsurvplot(ap_fit, risk.table = TRUE, conf.int = TRUE, pval = TRUE)
+ap_plot
